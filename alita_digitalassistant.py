95c2e7069b3c271f9fe6ae6be95ee17fa2e5f812
Faisal Ali <fzl.ali33@gmail.com>
2021-08-16 00:39:58 +0530

    Added logging

--- alita_digitalassistant.py
+++ alita_digitalassistant.py
@@ -1,6 +1,6 @@
 # Author: Faisal Ali
 # Creation Date: 7th Aug 2021
-# Version: 0.0.2
+# Version: 0.0.4
 # Revision Date: 15th Aug 2021
 
 # Importing libraries
@@ -8,6 +8,7 @@ import pyttsx3 as tts
 import speech_recognition as sr
 import datetime
 import random
+import logging
 
 class AlitaTextToSpeech:
     def __init__(self, speed = 190, voice = "Female"):
@@ -66,76 +67,95 @@ class AlitaSpeechRecog(AlitaTextToSpeech):
         super().__init__(voice = self.voice)
         rcg = sr.Recognizer()
         with sr.Microphone() as source:
-            print("Detecting any user input")
+            logger.info("Detecting any user input")
             rcg.pause_threshold = 1
             audio = rcg.listen(source)
         
         try:
-            print("Analyzing the user input")
+            logger.info("Analyzing the user input")
             query = rcg.recognize_google(audio, language="en-IN")
-            print(f"User Command: {query}")
+            logger.info(f"User Command: {query}")
         except Exception as error:
-            print(error)
-            print("AI Response: Unable to Recognize user input")
+            logger.info(error)
+            logger.info("AI Response: Unable to Recognize user input")
             self.communicate("Unable to Recognize user input")
             query = 'none'
         return query
 
 class AlitaActions(AlitaSpeechRecog, AlitaTextToSpeech):
+    """
+    Contains different methods for actions which assistant can perform
     
+    Return:
+    Voice Output
+    """
     def greetings(self):
         super().__init__(voice = self.voice)
         current_hour = datetime.datetime.now()
         morning = [ i for i in range(0,12) ]
         if current_hour.hour in morning:
-            print("AI Response: Good Morning !!")
+            logger.info("AI Response: Good Morning !!")
             self.communicate("Good Morning !!")
         elif current_hour.hour > 15:
-            print("AI Response: Good Evening !!")
+            logger.info("AI Response: Good Evening !!")
             self.communicate("Good Evening !!")
         else:
-            print("AI Response: Good Afternoon !!")
+            logger.info("AI Response: Good Afternoon !!")
             self.communicate("Good Afternoon !!")
-        print("AI Response: How can I help you ?")
+        logger.info("AI Response: How can I help you ?")
         self.communicate("How can I help you ?")
     
     def introduction(self):
         super().__init__(voice = self.voice)
-        print(f"AI Response: Hi, I'm Alita. Your digital personal assistant.")
+        logger.info(f"AI Response: Hi, I'm Alita. Your digital personal assistant.")
         self.communicate("Hi, I'm Alita. Your digital personal assistant.")
 
     def general_inquiry(self):
         super().__init__(voice = self.voice)
         replies = ["Somewhere between better and best.", "Oh, terrible, thank you so much!", "You go first. Then, we can compare.", "It's a secret.", "I'm doing great, how about you?"]
         response = random.choice(replies)
-        print(f"AI Response: {response}")
+        logger.info(f"AI Response: {response}")
         self.communicate(response)
     
     def fun_love(self):
         super().__init__(voice = self.voice)
         replies = ["I love ME too!", "Well, who doesn't?", "I get that a lot!", "You are day-dreaming again!", "Are you sure you aren’t sick or something?", "Yeah, thanks. I love myself too.", "Oh really, that's so funny.", "I sure wish the person who created me had given me the ability to understand this human emotion-love"]
         response = random.choice(replies)
-        print(f"AI Response: {response}")
+        logger.info(f"AI Response: {response}")
         self.communicate(response)
 
 if __name__ == "__main__":
+    #Setting up logging
+    home_dir = "C:/Users/fa1za/Documents/CODE/PYTHON/alita_digitalassistant/"
+    log_file = home_dir+"alita_digitalassistant.log"
+    logging.basicConfig(filename=log_file,level=logging.INFO, format='%(asctime)s : %(levelname)s -> %(message)s')
+    logger = logging.getLogger("Alita Digital Assistant")
+
+    # Displaying logs to the console
+    stream = logging.StreamHandler()
+    stream.setLevel(logging.INFO)
+    streamformat = logging.Formatter('%(asctime)s : %(levelname)s -> %(message)s')
+    stream.setFormatter(streamformat)
+    logger.addHandler(stream)
+
+    #Setting up assistant
     ai_obj = AlitaActions()
     ai_obj.greetings()
     while True:
         command = ai_obj.recognition().lower()
         try:
             calc = eval(command)
-            print(f"AI Response: {command} is {calc}")
+            logger.info(f"AI Response: {command} is {calc}")
             ai_obj.communicate(f"{command} is {calc}")
             continue
         except:
             pass
         if 'hi' in command or 'hello' in command:
-            print(f"AI Response: Hi there.")
+            logger.info(f"AI Response: Hi there.")
             ai_obj.communicate("Hi there.")
             continue
         if 'your name' in command:
-            print(f"AI Response: My name is Alita.")
+            logger.info(f"AI Response: My name is Alita.")
             ai_obj.communicate("My name is Alita.")
             continue
         elif 'introduce yourself' in command or 'who are you' in command or 'about yourself' in command:
@@ -150,10 +170,10 @@ if __name__ == "__main__":
         elif 'none' in command:
             continue
         elif '**' in command:
-            print("AI Response: Avoid using inappropriate words.")
+            logger.info("AI Response: Avoid using inappropriate words.")
             ai_obj.communicate("Avoid using inappropriate words.")
             continue
         else:
-            print("AI Response: Still learning, not able to answer the query")
+            logger.info("AI Response: Still learning, not able to answer the query")
             ai_obj.communicate("Still learning, not able to answer the query")
             continue
